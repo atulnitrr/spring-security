6592212d66a4d8f9a19288853185a1831ec9a514
atulnitrr <atulkumar458@gmail.com>
2019-08-11 22:08:48 +0530

    Spring expression language

--- src/main/java/com/atul/security/SecurityApplication.java
+++ src/main/java/com/atul/security/SecurityApplication.java
@@ -1,5 +1,8 @@
 package com.atul.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -26,6 +29,27 @@ public class SecurityApplication {
 
 }
 
+
+@Component
+class UUID {
+
+	public String getUUID() {
+		return java.util.UUID.randomUUID().toString();
+	}
+}
+
+
+@Component
+class Bar1 {
+
+	private Foo foo;
+	private final Logger logger = LoggerFactory.getLogger(getClass());
+
+	Bar1(final Foo foo, @Value("#{ UUID.getUUID() }") String uuid) {
+		this.foo = foo;
+		logger.info("uuid " +  uuid);
+	}
+}
 
 class Bar {
 
